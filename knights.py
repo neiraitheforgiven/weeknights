a46d5e0e419e5aace61262c6d2bce5237c44e4ae
john den otter <john.denotter@computronix.com>
2022-06-20 10:47:28 -0600

    Add dice

--- knights.py
+++ knights.py
@@ -10,21 +10,21 @@ class knight():
 		self.populate_dice()
 
     def populate_dice(self):
-    	self.dice = []
-    	for color in self.colors:
-    		self.dice.append(die(color))
+        self.dice = []
+        for color in self.colors:
+            self.dice.append(die(color))
 
-class die():
 
-	def __init__(self, color):
-		self.size = 6
-		self.rerolls = 0
-		self.min = 1
-		self.bonus = 0
+class die:
+    def __init__(self, color):
+        self.size = 6
+        self.rerolls = 0
+        self.min = 1
+        self.bonus = 0
         self.color = color
 
     def roll(self):
-    	dice.roll(f"1d{self.size}r{self.min}+{self.bonus}")
+        dice.roll(f"1d{self.size}r{self.min}+{self.bonus}")
 
 class monster():
 	
